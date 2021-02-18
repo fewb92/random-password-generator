502b6bed73d5f3a4ac693eb7046648ded61ef5bc
JSoto <sotojosh96@gmail.com>
2021-02-17 21:23:13 -0500

    copied code from html in supplied folder to index.html in the root folder and then edited links in index.html to js and css

--- supplied/Develop/script.js
+++ supplied/Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var specialChar = [ "!", "#", "$", "%", "&", ")", "(", "*", "+", "‘", "-", ".", "/", ":", "<", "=", ">", "?", "@", "[", "/","^", "`", "{", "|", "}", "~" ];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 // Write password to the #password input
 function writePassword() {
